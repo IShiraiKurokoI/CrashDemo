28d78c00c03798d343196dac071fe769e160b9c2
Shirai_Kuroko <2321047703@qq.com>
2023-01-07 15:38:22 +0800

    添加项目文件。

---
+++ CrashDemo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinUICommunity.Common.Helpers;
+using WinUICommunity.Common.Tools;
+using Microsoft.UI.Windowing;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CrashDemo
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public string TitleText = "CrashDemo";
+        public Grid ApplicationTitleBar => AppTitleBar;
+        internal static MainWindow Instance { get; private set; }
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            Instance = this;
+            TitleBarHelper.Initialize(this, TitleTextBlock, AppTitleBar, LeftPaddingColumn, IconColumn, TitleColumn, LeftDragColumn, SearchColumn, RightDragColumn, RightPaddingColumn);
+            Localizer.Get().InitializeWindow(Root, Content);
+
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.SetIcon("Assets/favicon.ico");
+            appWindow.Resize(new Windows.Graphics.SizeInt32(1400, 800));
+        }
+    }
+}
